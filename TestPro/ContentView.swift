6125b573b5b8980c964036ff10ce7e137b7b30dc
mohammadTahreem <68543254+mohammadTahreem@users.noreply.github.com>
2021-08-07 14:28:35 +0530

    init commit

---
+++ TestPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestPro
+//
+//  Created by Mohammad Tahreem Qadri on 07/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestPro/TestProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProApp.swift
+//  TestPro
+//
+//  Created by Mohammad Tahreem Qadri on 07/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
